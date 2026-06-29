179fa167b71a6b28cab64db758204eb50a861403
Alex <alex.m.bazzelle@gmail.com>
2026-06-29 16:27:30 -0500

    Similarity Sample

--- axelrodVoice/networkGraph.py
+++ axelrodVoice/networkGraph.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 
 class SimilarityGraph:
-    def __init__(self):
+    def __init__(self, filename: str):
         self.full_data = pd.DataFrame(columns=[
             "subject", "opponent", "seed", "num_rounds", "first_round", "last_round",
             "per_c", "per_C", "per_cC", "per_cD", "per_dC", "per_dD",
@@ -19,6 +19,7 @@ class SimilarityGraph:
         self.opponents: list[str] = []
         self.partitions = pd.DataFrame(columns=["cluster"])
         self.frequency_table = pd.DataFrame(columns=[])
+        self.load_data(filename)
 
     @staticmethod
     def one_match_to_one_entry(match: axlVoice.Match, first_round=1, last_round=200):
@@ -60,36 +61,39 @@ class SimilarityGraph:
         return match_header + behaviors
 
     # Convert a list of subjects and opponents into a full dataframe
-    def generate_data(self,
-                      subjects: list[axlVoice.Player], opponents: list[axlVoice.Player],
+    @staticmethod
+    def generate_data(subjects: list[axlVoice.Player], opponents: list[axlVoice.Player], filename: str,
                       first_round=1, last_round=200, seeds = range(5),
-                      filename: str | None = None) -> pd.DataFrame:
-        #print(f"Total matches: {len(subjects)*len(opponents)*len(seeds)}")
+                      ) -> pd.DataFrame:
+
+        full_data = pd.DataFrame(columns=[
+            "subject", "opponent", "seed", "num_rounds", "first_round", "last_round",
+            "per_c", "per_C", "per_cC", "per_cD", "per_dC", "per_dD",
+            "avg_score", "avg_net_score"
+        ])
 
         counter = 0
         for subject in subjects:
             counter += 1
             print(f"...playing subject {counter}/{len(subjects)}")
-            self.subjects.append(subject.name)
             for opponent in opponents:
-                self.opponents.append(opponent.name)
                 for seed_i in seeds:
-                    #print(f"Playing {subject} vs {opponent} ({seed_i})")
                     match = axlVoice.Match( (subject, opponent), turns=last_round, seed=seed_i)
                     match.play()
-                    entry_row = self.one_match_to_one_entry(match, first_round, last_round)
-                    self.full_data.loc[len(self.full_data)] = entry_row
+                    entry_row = SimilarityGraph.one_match_to_one_entry(match, first_round, last_round)
+                    full_data.loc[len(full_data)] = entry_row
 
-        # Save to csv. Recommended.
-        if filename: self.full_data.to_csv(filename, index=False)
+        # Save to csv. Required.
+        full_data.to_csv(filename, index=False)
 
-        return self.full_data
+        return full_data
 
     # Reload existing data from csv file
-    def reload_data(self, filename: str):
+    def load_data(self, filename: str):
         self.full_data = pd.read_csv(filename)
         self.subjects = self.full_data.subject.unique()
         self.opponents = self.full_data.opponent.unique()
+        return self.full_data
 
 
     # Build partition set
@@ -139,16 +143,18 @@ class SimilarityGraph:
                 cluster_counter += len(set(cluster_ids))
                 result_df = pd.DataFrame({"cluster_id": cluster_ids, "subject": batch_df["subject"]}).sort_values(by=["cluster_id", "subject"])
 
-                # Insert into P, the set of S_i's.
+                # Insert into P, the set of S_o's.
                 all_clusters = pd.concat([all_clusters,result_df]); batch_counter += 1
 
-        # Remove duplicated clusters, leaving only unique S_i's
+        # Remove duplicated clusters, leaving only unique S_o's
         self.partitions = (all_clusters
                            .groupby("cluster_id")["subject"]
-                           .apply(frozenset)
+                           .apply(frozenset) # Put clustered subjects together
                            .reset_index()
                            .set_axis(["cluster_id","cluster"], axis=1)
-                           .drop_duplicates(subset="cluster")
+                           .drop_duplicates(subset="cluster") # Remove duplicated clusters
+                           .loc[lambda df: df["cluster"].apply(len) > 1] # Remove singleton clusters
+                           .reset_index(drop=True)
                            .drop(columns=["cluster_id"]))
 
         return self.partitions
@@ -165,7 +171,6 @@ class SimilarityGraph:
                 count_occurrences.loc[s2, s1] += 1
 
         # Build the probability frequency table. Index I,J is P(J|I) for chance of sharing cluster.
-        # This table is akin to a Markov Chain
         self.frequency_table = pd.DataFrame(0.0, index=self.subjects, columns=self.subjects)
         for s1,s2 in combinations(self.subjects, 2):
             self.frequency_table.loc[s1,s2] = count_occurrences.loc[s1,s2] / count_occurrences.loc[s1,s1]
@@ -174,13 +179,13 @@ class SimilarityGraph:
         return self.frequency_table
 
     # Create the similarity graph
-    def draw_similarity_graph(self, edge_outlier_thresh: float=0, show=True, filename: str | None = None) -> None:
+    def draw_similarity_graph(self, edge_outlier_thresh: float=0, show=True, filename: str | None = None, adj_matrix: pd.DataFrame | None = None) -> None:
         # Get the variables
-        group_freq_tbl = self.partitions_to_freq_tbl()
+        if adj_matrix is None: adj_matrix = self.partitions_to_freq_tbl()
 
         # Add edges to graph
         plt.close()
-        G = nx.from_pandas_adjacency(group_freq_tbl, create_using=nx.DiGraph)
+        G = nx.from_pandas_adjacency(adj_matrix, create_using=nx.DiGraph)
         sorted_edges = sorted(G.edges(data=True), key=lambda edge: edge[2].get('weight', 1))
         G = nx.from_edgelist(sorted_edges, create_using=nx.DiGraph)
 
